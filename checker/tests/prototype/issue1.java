dc0c9393de1c4ef172a2d674dce1319de651bfda
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-07-30 21:42:32 +0530

    Add test case

---
+++ checker/tests/prototype/issue1.java
@@ -0,0 +1,14 @@
+import org.checkerframework.checker.prototype.qual.*;
+
+/* The following test case should not pass as I have made the checker only relevant to
+Byte.class, Integer.class, Short.class, Long.class using RelevantJavaTypes  */
+class issue1 {
+
+    @Unsigned float f1;
+    @Unsigned double d1;
+
+    void m1() {
+        @Unsigned float f2 = f1;
+        @Unsigned double d2 = d1;
+    }
+}
