d2493bf90f9d7122d1825b562b04373bcb2b6a33
Michael Ernst <mdernst@amazon.com>
2019-07-30 08:45:18 -0700

    Add documentation

--- checker/src/main/java/org/checkerframework/checker/prototype/qual/Signed.java
+++ checker/src/main/java/org/checkerframework/checker/prototype/qual/Signed.java
@@ -9,6 +9,12 @@ import org.checkerframework.framework.qual.DefaultFor;
 import org.checkerframework.framework.qual.SubtypeOf;
 import org.checkerframework.framework.qual.TypeKind;
 
+/**
+ * The value is to be interpreted as signed. That is, if the most significant bit in the bitwise
+ * representation is set, then the bits should be interpreted as a negative number.
+ *
+ * @checker_framework.manual #signedness-checker Signedness Checker
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
