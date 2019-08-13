1958af2de8761c143d785421699dcd5769b58949
Kartikeya Goswami <kartikeyagoswami20@gmail.com>
2019-08-13 18:39:17 +0530

    Remove documentation

--- framework/src/main/java/org/checkerframework/common/value/qual/EnsuresIntRangeIf.java
+++ framework/src/main/java/org/checkerframework/common/value/qual/EnsuresIntRangeIf.java
@@ -8,36 +8,19 @@ import org.checkerframework.framework.qual.ConditionalPostconditionAnnotation;
 import org.checkerframework.framework.qual.InheritedAnnotation;
 import org.checkerframework.framework.qual.QualifierArgument;
 
-/**
- * Indicates that the value of the given expression is a sequence containing at least the given
- * number of elements, if the method returns the given result (either true or false).
- *
- * <p>When the annotated method returns {@code result}, then all the expressions in {@code
- * expression} are considered to be {@code MinLen(targetValue)}.
- *
- * @see MinLen
- * @checker_framework.manual #constant-value-checker Constant Value Checker
- */
 @ConditionalPostconditionAnnotation(qualifier = IntRange.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 @InheritedAnnotation
 public @interface EnsuresIntRangeIf {
-    /**
-     * Java expression(s) that are a sequence with the given minimum length after the method returns
-     * the given result.
-     *
-     * @checker_framework.manual #java-expressions-as-arguments Syntax of Java expressions
-     */
+
     String[] expression();
 
-    /** The return value of the method that needs to hold for the postcondition to hold. */
     boolean result();
 
-    /** The minimum number of elements in the sequence. */
     @QualifierArgument("from")
     long from() default Long.MIN_VALUE;
-    /** Largest value in the range, inclusive. */
+
     @QualifierArgument("to")
     long to() default Long.MAX_VALUE;
 }
